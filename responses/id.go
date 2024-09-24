9fcd5bc38f428255d26444e6a58d62bdc5a096eb
Selphine <selphine@selphine.com>
2024-09-24 13:51:46 +0800

    fix: id resp

---
+++ responses/id.go
@@ -0,0 +1,26 @@
+package responses
+
+import (
+	"github.com/se1phine/go-imap"
+)
+
+type ID struct {
+	ResStr []string
+}
+
+func (r *ID) Handle(resp imap.Resp) error {
+	name, fields, ok := imap.ParseNamedResp(resp)
+	if !ok || name != "ID" {
+		return ErrUnhandled
+	} else if len(fields) < 1 {
+		return errNotEnoughFields
+	}
+	res := fields[0].([]interface{})
+	var resStrSlices []string
+	for i := 0; i < len(res); i++ {
+		resStrSlices = append(resStrSlices, res[i].(string))
+	}
+	r.ResStr = resStrSlices
+
+	return nil
+}
